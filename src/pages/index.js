177a46af890fdaf3b1592a1425cbfe6f59ed0639
Charlie Hoey <me@charliehoey.com>
2019-03-17 21:18:41 -0400

    added placeholder photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+
+import IMAGE_1 from '../assets/152495main_image_feature_616b_ys.jpg'
+import IMAGE_2 from '../assets/9460194568_b8f0110e97_o.jpg'
+import IMAGE_3 from '../assets/as11-40-5944.jpg'
+import IMAGE_4 from '../assets/AS11-44-6549HR.jpg'
+import IMAGE_5 from '../assets/AS11-44-6581HR.jpg'
+import IMAGE_6 from '../assets/ksc-69p-670.jpg'
 
 const GlobalStyle = createGlobalStyle`
     font: "open-sans", sans-serif;
@@ -12,7 +19,12 @@ export default class Slideshow extends Component {
     return (
       <div>
         <GlobalStyle />
-        HELLO
+        <img src={IMAGE_1} alt="image1" />
+        <img src={IMAGE_2} alt="image2" />
+        <img src={IMAGE_3} alt="image3" />
+        <img src={IMAGE_4} alt="image4" />
+        <img src={IMAGE_5} alt="image5" />
+        <img src={IMAGE_6} alt="image6" />
       </div>
     )
   }
